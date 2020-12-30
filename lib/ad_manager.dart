4646e3cb1b1a8aa440340ea3fabc2e562e78dbd0
a a <a931eric@gmail.com>
2020-12-31 01:26:54 +0800

    ad failed to load with my id

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -5,8 +5,8 @@ class AdManager {
 
   static String get appId {
     if (Platform.isAndroid) {
-      //return "ca-app-pub-2705845233990833~8915655196";
-      return "ca-app-pub-3940256099942544~4354546703";//test
+      return "ca-app-pub-2705845233990833~8915655196";
+      //return "ca-app-pub-3940256099942544~4354546703";//test
     } else if (Platform.isIOS) {
       return "ca-app-pub-3940256099942544~2594085930";
     } else {
@@ -16,8 +16,8 @@ class AdManager {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      //return "ca-app-pub-2705845233990833/5347751752";
-      return "ca-app-pub-3940256099942544/6300978111";//test
+      return "ca-app-pub-2705845233990833/5347751752";
+      //return "ca-app-pub-3940256099942544/6300978111";//test
     } else if (Platform.isIOS) {
       return "ca-app-pub-3940256099942544/4339318960";
     } else {
